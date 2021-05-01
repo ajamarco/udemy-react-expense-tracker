7155c4aa5dd52af04c09de592491cb33117eb887
alexandre jamarco <alexandre.j@gmail.com>
2021-05-01 21:39:49 +0100

    added expenselist css and jsx files

--- src/components/jsx/ExpenseItem.js
+++ src/components/jsx/ExpenseItem.js
@@ -9,22 +9,14 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 
 function ExpenseItem({ title, date, amount }) {
-    const [expenseTitle, setTitle] = useState(title);
-
-    //handle a button click
-    //------------------------------
-    const handleClick = (e) => {
-        setTitle('updated!');
-    };
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={date} />
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">$ {amount}</div>
             </div>
-            <button onClick={handleClick}>Change Title</button>
         </Card>
     );
 }
